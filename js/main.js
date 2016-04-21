2ea77e0c011937510001c3410c71b699d43d82b5
Jonathan Caicedo <jbirdcaicedo@yahoo.com>
2016-04-21 19:04:28 -0400

    Added jQuery. Availability is now appended onto dininghall divs.

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var available = "<div class = \"available\">	Available <img class = \"greencheck\" src = \"img/greencheck.png\"> </div>";
+$(Blitman).append(available + "<div class = \"updated\">Last updated: Testing</div>" + "<div class = \"timestamp\">Available since: Testing</div>");	
+$(BARH).append(available + "<div class = \"updated\">Last updated: Testing</div>" + "<div class = \"timestamp\">Available since: Testing</div>");
+$(Commons).append(available + "<div class = \"updated\">Last updated: Testing</div>" + "<div class = \"timestamp\">Available since: Testing</div>");
+$(Sage).append(available + "<div class = \"updated\">Last updated: Testing</div>" + "<div class = \"timestamp\">Available since: Testing</div>");	
