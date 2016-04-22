91dd89cbcb6055c47022b4370bd91989ec839a92
Jonathan Caicedo <jbirdcaicedo@yahoo.com>
2016-04-22 18:53:41 -0400

    Updated jQuery. Added more error checking. Cleaned time zone info from JSON (everything is now assumed UTC-5)

--- js/main.js
+++ js/main.js
@@ -1,33 +1,50 @@
-$.getJSON("http://localhost:8080/data.json", function(json) 
-{
-
-    var data = $.parseJSON(JSON.stringify(json));
-    console.log(data.id);
-
-    switch (data.id)
-	{
-		case 0: 
-			if (data.available == true)
-			{
-				$(Blitman).append(available + "<div class = \"updated\">Last updated: " + data.last_updated + "</div>" + "<div class = \"timestamp\">Available since: " + data.availability + "</div>");	
-			}
-
-			else if (data.available == false)
-			{
-				$(Blitman).append(unavailable + "<div class = \"updated\">Last updated: " + data.last_updated + "</div>" + "<div class = \"timestamp\">Unavailable since: " + data.availability + "</div>");	
-			}
-		break;
-
-		default:
-		console.log("Error.");
-	}
-});
-
-
-	
-
 var available = "<div class = \"available\">	Available <img class = \"greencheck\" src = \"img/greencheck.png\"> </div>";
 var unavailable = "<div class = \"unavailable\">	Unavailable <img class = \"redx\" src = \"img/redx.png\"> </div>";
-$(BARH).append(available + "<div class = \"updated\">Last updated: Testing</div>" + "<div class = \"timestamp\">Available since: Testing</div>");
-$(Commons).append(available + "<div class = \"updated\">Last updated: Testing</div>" + "<div class = \"timestamp\">Available since: Testing</div>");
-$(Sage).append(unavailable + "<div class = \"updated\">Last updated: Testing</div>" + "<div class = \"timestamp\">Unavailable since: Testing</div>");	
+var connectionError = "<div class = \"unavailable\">Connection Error</div>"
+initalize();
+
+
+function initalize()
+{
+	var data = null;
+
+	$.getJSON("http://localhost:8080/data.json", function(data)
+	{
+
+    	data = $.parseJSON(JSON.stringify(data));
+    	evalulate(data);
+    	
+	})
+
+	.fail(function(data)
+	{
+		$(".dininghall").append(connectionError);
+	});
+}
+
+function evalulate(data)
+{
+	if (data != null)
+	{
+
+		switch (data.id)
+		{
+			case 0: 
+				if (data.available == true)
+				{
+					$(Blitman).append(available + "<div class = \"updated\">Last updated: " + data.last_updated + "</div>" + "<div class = \"timestamp\">Available since: " + data.availability + "</div>");	
+				}
+
+				else if (data.available == false)
+				{
+					$(Blitman).append(unavailable + "<div class = \"updated\">Last updated: " + data.last_updated + "</div>" + "<div class = \"timestamp\">Unavailable since: " + data.availability + "</div>");	
+				}
+			break;
+
+			default:
+			break;
+			
+		}
+	}
+
+}	
