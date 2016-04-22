4cc72dce9ced432169a902e56b7605b921896ebc
Jonathan Caicedo <jbirdcaicedo@yahoo.com>
2016-04-21 20:34:52 -0400

    Added unavailable meesage and red X.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var available = "<div class = \"available\">	Available <img class = \"greencheck\" src = \"img/greencheck.png\"> </div>";
+var unavailable = "<div class = \"unavailable\">	Unavailable <img class = \"redx\" src = \"img/redx.png\"> </div>";
 $(Blitman).append(available + "<div class = \"updated\">Last updated: Testing</div>" + "<div class = \"timestamp\">Available since: Testing</div>");	
 $(BARH).append(available + "<div class = \"updated\">Last updated: Testing</div>" + "<div class = \"timestamp\">Available since: Testing</div>");
 $(Commons).append(available + "<div class = \"updated\">Last updated: Testing</div>" + "<div class = \"timestamp\">Available since: Testing</div>");
-$(Sage).append(available + "<div class = \"updated\">Last updated: Testing</div>" + "<div class = \"timestamp\">Available since: Testing</div>");	
+$(Sage).append(unavailable + "<div class = \"updated\">Last updated: Testing</div>" + "<div class = \"timestamp\">Unavailable since: Testing</div>");	
