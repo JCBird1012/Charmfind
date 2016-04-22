868c45f145a047f64cb4cd967320c356950582cf
Jonathan Caicedo <jbirdcaicedo@yahoo.com>
2016-04-22 00:17:51 -0400

    Added some rudimentary JSON parsing.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,33 @@
+$.getJSON("http://localhost:8080/data.json", function(json) 
+{
+
+    var data = $.parseJSON(JSON.stringify(json));
+    console.log(data.id);
+
+    switch (data.id)
+	{
+		case 0: 
+			if (data.available == true)
+			{
+				$(Blitman).append(available + "<div class = \"updated\">Last updated: " + data.last_updated + "</div>" + "<div class = \"timestamp\">Available since: " + data.availability + "</div>");	
+			}
+
+			else if (data.available == false)
+			{
+				$(Blitman).append(unavailable + "<div class = \"updated\">Last updated: " + data.last_updated + "</div>" + "<div class = \"timestamp\">Unavailable since: " + data.availability + "</div>");	
+			}
+		break;
+
+		default:
+		console.log("Error.");
+	}
+});
+
+
+	
+
 var available = "<div class = \"available\">	Available <img class = \"greencheck\" src = \"img/greencheck.png\"> </div>";
 var unavailable = "<div class = \"unavailable\">	Unavailable <img class = \"redx\" src = \"img/redx.png\"> </div>";
-$(Blitman).append(available + "<div class = \"updated\">Last updated: Testing</div>" + "<div class = \"timestamp\">Available since: Testing</div>");	
 $(BARH).append(available + "<div class = \"updated\">Last updated: Testing</div>" + "<div class = \"timestamp\">Available since: Testing</div>");
 $(Commons).append(available + "<div class = \"updated\">Last updated: Testing</div>" + "<div class = \"timestamp\">Available since: Testing</div>");
 $(Sage).append(unavailable + "<div class = \"updated\">Last updated: Testing</div>" + "<div class = \"timestamp\">Unavailable since: Testing</div>");	
