b9817317bfe1f44987854b744c79367af6e0b277
Jonathan Elias Caicedo <jbirdcaicedo@yahoo.com>
2016-04-23 22:41:19 -0400

    Added update button... Doesn't do anything for now. Fairly tenative.

--- js/main.js
+++ js/main.js
@@ -21,22 +21,23 @@ function initalize()
 
 function evalulate(data)
 {
-	var available, unavailable;
+	var available, unavailable, updateButton;
 
 	for (var i = 0; i <= 3; ++i)
 	{
 		//TODO - Remove these from the loop... Have to figure out the best way to do that.
 		available = "<div class = \"available\">Available <img class = \"greencheck\" src = \"img/greencheck.png\"> </div> <div class = \"updated\">Last updated: " + data.results[i].last_updated + "</div>" + "<div class = \"timestamp\">Available since: " + data.results[i].availability + "</div>";
 		unavailable = "<div class = \"unavailable\">Unavailable <img class = \"redx\" src = \"img/redx.png\"> </div> <div class = \"updated\">Last updated: " + data.results[i].last_updated + "</div>" + "<div class = \"timestamp\">Unvailable since: " + data.results[i].availability + "</div>";
+		updateButton = "<button>Update</button>";
 
 		if (data.results[i].available == true)
 		{
-			$("#" + data.results[i].dining_hall).append(available);
+			$("#" + data.results[i].dining_hall).append(available + updateButton);
 		}
 
 		else
 		{
-			$("#" + data.results[i].dining_hall).append(unavailable);
+			$("#" + data.results[i].dining_hall).append(unavailable + updateButton);
 		}
 	}
 
